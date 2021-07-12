963d04ab90117f988447e9be60a6266c588a7412
Arron Speake <arron.speake@bluefruit.co.uk>
2021-07-12 12:52:10 +0100

    Add Loadstone CLI feature file and unimplemented steps.

---
+++ steps/steps.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+from behave import *
+
+@given("the custom greeting of Loadstone has been set to")
+def step_impl(context):
+    assert False # TODO
+
+@given("an STM32F412G connected via USART")
+def step_impl(context):
+    assert False # TODO
+
+@given("Loadstone has been configured with a golden bank and serial recovery mode")
+def step_impl(context):
+    assert False # TODO
+
+@given("only Loadstone is loaded on the dev kit")
+def step_impl(context):
+    assert False # TODO
+
+@given("Loadstone has been configured with serial recovery mode and no golden bank")
+def step_impl(context):
+    assert False # TODO
+
+@given("Loadstone is in serial recovery mode")
+def step_impl(context):
+    assert False # TODO
+
+@given("Loadstone has been configured to manage bank 1 internal bootable 100kB, bank 2 internal 100kB, bank 3 external golden 100kB, bank 4 external 100kB")
+def step_impl(context):
+    assert False # TODO
+
+@given("a valid firmware image is in the boot bank")
+def step_impl(context):
+    assert False # TODO
+
+@given("an STM32F412G")
+def step_impl(context):
+    assert False # TODO
+
+@given("an indentical firmware image is in bank 1")
+def step_impl(context):
+    assert False # TODO
+
+@when("the dev kit is powered on")
+def step_impl(context):
+    assert False # TODO
+
+@when("a golden firmware image is uploaded using XMODEM")
+def step_impl(context):
+    assert False # TODO
+
+@when("a non-golden firmware image is uploaded using XMODEM")
+def step_impl(context):
+    assert False # TODO
+
+@then("the following is printed to the CLI")
+def step_impl(context):
+    assert False # TODO
+
+@then("")
+def step_impl(context):
+    assert False # TODO
